125f89e6d8a5aaffb91712bd846ea9492925e53b
Bhavin8289 <bupadhyay277@gmail.com>
2024-06-19 00:09:24 +0530

    Initial Commit

---
+++ Weather-accue/Weather_accueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_accueApp.swift
+//  Weather-accue
+//
+//  Created by MacBookPro on 29/03/1946 Saka.
+//
+
+import SwiftUI
+
+@main
+struct Weather_accueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
