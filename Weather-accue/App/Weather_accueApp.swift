6505458eef3e975c963b33576069cdb45d62fa15
Bhavin8289 <bupadhyay277@gmail.com>
2024-06-19 00:41:16 +0530

    -

--- Weather-accue/App/Weather_accueApp.swift
+++ Weather-accue/App/Weather_accueApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Weather_accueApp: App {
     var body: some Scene {
         WindowGroup {
-            WelcomeScreen()
+            ContentView()
         }
     }
 }
--- Weather-accue/Views/ContentView.swift
+++ Weather-accue/Views/ContentView.swift
@@ -7,7 +7,10 @@
 
 import SwiftUI
 
-struct WelcomeScreen: View {
+struct ContentView: View {
+    
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -21,6 +24,6 @@ struct WelcomeScreen: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        WelcomeScreen()
+        ContentView()
     }
 }
